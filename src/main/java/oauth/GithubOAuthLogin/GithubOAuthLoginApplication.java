02a85d481209f64a1a710df00041344d0383bbb4
sungmingt <qotjdals8313@naver.com>
2022-08-15 20:26:41 +0900

    first commit

---
+++ src/main/java/oauth/GithubOAuthLogin/GithubOAuthLoginApplication.java
@@ -0,0 +1,13 @@
+package oauth.GithubOAuthLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubOAuthLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubOAuthLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oauth/GithubOAuthLogin/GithubOAuthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth.GithubOAuthLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubOAuthLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
